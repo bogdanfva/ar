ad8efe8c7aa6d001bedaf1e4bb9a58c305b96d9b
bogdanfva <bogdanfva@gmail.com>
2022-06-02 19:25:43 +0700

    Add files via upload

---
+++ MenuControls.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour
+{
+    public void MENUS()
+    {
+        SceneManager.LoadScene("MEN");
+    }
+
+    public void ARKRAS()
+    {
+        SceneManager.LoadScene("ARKA");
+    }
+
+    public void PSUR()
+    {
+        SceneManager.LoadScene("SURIKOV");
+    }
+
+    public void PPOZD()
+    {
+        SceneManager.LoadScene("POZD");
+    }
+
+    public void PPG()
+    {
+        SceneManager.LoadScene("PUSHKIN");
+    }
+
+    public void PFOTAP()
+    {
+        SceneManager.LoadScene("FOTOAPA");
+    }
+
+}
+
